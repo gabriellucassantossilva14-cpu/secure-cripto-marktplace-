09924669383578a96cd38d24e6db2bcc6556bda8
gabriellucassantossilva14-cpu <gabriellucassantossilva14@gmail.com>
2026-03-24 23:54:21 -0300

    Add MarketplaceToken contract with mint and burn functions

---
+++ contracts/MarketplaceToken.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MarketplaceToken is ERC20, Ownable {
+    constructor() ERC20("Marketplace Token", "MRKT") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+    
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+    
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+    
+    function burnFrom(address account, uint256 amount) public {
+        uint256 currentAllowance = allowance(account, msg.sender);
+        require(currentAllowance >= amount, "ERC20: burn amount exceeds allowance");
+        _approve(account, msg.sender, currentAllowance - amount);
+        _burn(account, amount);
+    }
+}
